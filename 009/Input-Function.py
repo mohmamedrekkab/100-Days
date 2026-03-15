11936457557ff3f7cb216f858ddcf53cb207da31
mohmamedrekkab <127940883+mohmamedrekkab@users.noreply.github.com>
2026-03-15 18:33:37 +0100

    1st-day2

--- 009/Input-Function.py
+++
@@ -1,2 +0,0 @@
-#This code print the number of character in a user's name.
-print(len(input("What is your name? ")))
---
+++ 1st-Day-009/Input-Function.py
@@ -0,0 +1,7 @@
+#This code print the number of character in a user's name.
+print(len(input("What is your name? ")))
+
+# notes
+#If input was "Mohamed"
+#1st: print(len("Mohamed"))
+#2nd: print(4)
