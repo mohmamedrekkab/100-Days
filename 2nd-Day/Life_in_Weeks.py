57cad13b960e6e4969418cdfadfc95ec5ecf627e
mohmamedrekkab <127940883+mohmamedrekkab@users.noreply.github.com>
2026-03-16 10:59:43 +0100

    2nd-Day_3rdExample

---
+++ 2nd-Day/Life_in_Weeks.py
@@ -0,0 +1,6 @@
+age=int(input("What's your current age?:  "))
+age_left= 90 - age
+day= age_left *365
+week = age_left *52
+month = age_left *12
+print(f"you have {day} days, {week} weeks and {month} months left")
