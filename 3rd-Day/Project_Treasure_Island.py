fd538c61cf0d5b07218cdf6e082960aa1e85137a
mohmamedrekkab <127940883+mohmamedrekkab@users.noreply.github.com>
2026-03-17 12:01:24 +0100

    Update Project_Treasure_Island.py

--- 3rd-Day/Project_Treasure_Island.py
+++ 3rd-Day/Project_Treasure_Island.py
@@ -1,10 +1,10 @@
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure")
-direction = input("You are at a cross road. Where do you want to go? Type 'left' or 'right': \n")
+direction = input("You are at a cross road. Where do you want to go? Type 'left' or 'right': \n").lower()
 if direction == "left":
-    inlake = input("You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.: \n")
+    inlake = input("You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.: \n").lower()
     if inlake == "wait":
-        door_color= input("You arrive at the island unharmed. There is a house with 3 doors. one re, one yellow and blue. wich colour do you choose?: \n")
+        door_color= input("You arrive at the island unharmed. There is a house with 3 doors. one re, one yellow and blue. wich colour do you choose?: \n").lower()
         if door_color == "yellow":
             print("You win")
         else: print("Game over")
