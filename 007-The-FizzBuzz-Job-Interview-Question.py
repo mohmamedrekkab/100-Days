252fb844daf7ec468378b00966746884d02111ec
mohmamedrekkab <127940883+mohmamedrekkab@users.noreply.github.com>
2026-03-25 09:02:12 +0100

    Update 007-The-FizzBuzz-Job-Interview-Question.py

--- 007-The-FizzBuzz-Job-Interview-Question.py
+++ 007-The-FizzBuzz-Job-Interview-Question.py
@@ -1,9 +1,9 @@
 for number in range(1,101):
     
     if number % 3 == 0 and number % 5 == 0:
-        print("FizzBuzz")
+        print(f"FizzBuzz, {number}/3 = {number/3}, {number}/5= {number/5}" )
     elif number %3 ==0 and number % 5 != 0:
-        print("Fizz")
+        print(f"Fizz, {number}/3  = {number/3}")
     elif number %3 != 0 and number %5 ==0:
-        print("Buzz")
+        print(f"Buzz, {number}/5  = {number/5}")
     else: print(number)
