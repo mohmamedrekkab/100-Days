84288dae46cbba9a0749dade6a998249c49a339c
mohamedrekkab <127940883+mohmamedrekkab@users.noreply.github.com>
2026-04-02 09:19:09 +0100

    Update day-8-start.py

--- 8th-Day/day-8-start.py
+++ 8th-Day/day-8-start.py
@@ -4,3 +4,16 @@ def greet():
     print("Isn't the weather nice today?")
 
 greet()
+
+def greet_with_name(name):
+    print(f"Hallow {name}")
+    print(f"How do you do {name} ?")
+    print(f"Isn't the weather nice today {name}?")
+
+greet_with_name("Mohamed")
+
+def greet_with(name, location):
+    print(f"Hallow {name}")
+    print(f"What is it like in {location} ?")
+
+greet_with("Mohamed","Hamburg")
