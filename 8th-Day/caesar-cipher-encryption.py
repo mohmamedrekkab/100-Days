810f0b710c2fd067d38dd58f3d2807ce0980b8d7
mohamedrekkab <127940883+mohmamedrekkab@users.noreply.github.com>
2026-04-02 15:43:09 +0100

    Create caesar-cipher-encryption.py

---
+++ 8th-Day/caesar-cipher-encryption.py
@@ -0,0 +1,59 @@
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+           'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
+           'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 
+           'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 
+           's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+letters2=[]
+
+coding=input("Type 'encode' to encrypt, type 'decode' to decrypt\n")
+phrase=input("Type you message:\n").lower()
+
+
+shiftnumber= int(input("Type the schift number:\n"))
+
+def encryption(p, n):
+    phrase2=""
+    if p=="encode":
+        for n in range(n, 26+n):
+            letters2.append(letters[n])
+        #print(letters2)
+        for i in range(len(phrase)):
+            for j in range(26):
+                if phrase[i] == letters[j]:
+                #print(phrase[i])
+                #print(letters2[j])
+                    phrase2+=letters2[j]
+            if phrase[i]==" ":
+                phrase2+=" "
+                #phrase2[i].replace=letters2[j]
+        print(phrase2)
+    
+    elif p=="decode":
+        for n in range(n, 27+n):
+            letters2.append(letters[n])
+    #print(letters2)
+        for i in range(len(phrase)):
+            for j in range(26):
+                if phrase[i] == letters2[j]:
+                #print(phrase[i])
+                #print(letters2[j])
+                    phrase2+=letters[j]
+            if phrase[i]==" ":
+                phrase2+=" "
+                #phrase2[i].replace=letters2[j]        
+    
+        print(phrase2)
+
+    
+    #print(phrase2)    
+
+encryption(coding, shiftnumber)
+respons=input("Do you want to continue y / n: ")
+if respons== "y":
+    coding=input("Type 'encode' to encrypt, type 'decode' to decrypt\n")
+    phrase=input("Type you message:\n").lower()
+    encryption(coding, shiftnumber)
+
+
+
+
